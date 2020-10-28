5445e217ef4186283d3fbbcaa8dd784e66943418
Tonio <antoniomaita64@gmail.com>
2020-10-28 15:36:50 +0100

    deploy on firebase, add link info

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -1,6 +1,8 @@
 import React, {Fragment, useEffect, useState} from 'react'
-import {GiTrophyCup} from "react-icons/gi"
+import { GiTrophyCup } from "react-icons/gi"
 import Loader from "../Loader/"
+import Modal from '../Modal/'
+import axios from 'axios'
 
 
 const QuizOver = React.forwardRef((props, ref) => {
@@ -14,11 +16,80 @@ const QuizOver = React.forwardRef((props, ref) => {
         loadLevelQuestions
     } = props;
 
+    const API_PUBLIC_KEY = process.env.REACT_APP_MARVEL_API_KEY;
+    
+    const hash = process.env.REACT_APP_MARVEL_HASH_KEY;
+
     const [asked, setAsked] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
+    const [characterInfos, setCharacterInfos ] = useState([]);
+    const [loading, setLoading ] = useState(true);
 
     useEffect(() => {
+
         setAsked(ref.current)
+
+        if (localStorage.getItem('marvelStorageDate')) {
+            const date = localStorage.getItem('marvelStorageDate');
+            checkDataAge(date)
+        }
+
     }, [ref])
+
+    const checkDataAge = date => {
+
+        const today = Date.now();
+        const timeDifference = today - date;
+
+        const daysDifference = timeDifference /(1000 *3600 * 24);
+
+        if (daysDifference >= 15) {
+            localStorage.clear();
+            localStorage.setItem('marvelStorageDate', Date.now());
+        }
+
+    }
+
+    const showModal = id => {
+        setOpenModal(true);
+
+        if(localStorage.getItem(id)) {
+
+            setCharacterInfos(JSON.parse(localStorage.getItem(id)));
+            setLoading(false);
+
+        }else {
+
+            axios
+            .get(`https://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=${API_PUBLIC_KEY }&hash=${hash}`)
+            .then(response => {
+
+                    setCharacterInfos(response.data);
+                    setLoading(false);
+
+                    localStorage.setItem(id, JSON.stringify(response.data));
+                    if (!localStorage.getItem('marvelStorageDate')) {
+                        localStorage.setItem('marvelStorageDate', Date.now());
+                    }
+
+
+
+            })
+            .catch(err => console.log(err))
+
+        }
+        
+    }
+
+    const hideModal = () => {
+        setOpenModal(false);
+        setLoading(true);
+    }
+
+    const capitalizeFirestletter = string => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+
+    }
     
     const averageGrade = maxQuestions /2;
 
@@ -96,7 +167,13 @@ const QuizOver = React.forwardRef((props, ref) => {
                                             <td>{question.question}</td>
                                             <td>{question.answer}</td>
                                             <td>
-                                                <button className="btnInfo">Infos</button>
+                                                <button 
+                                                    className="btnInfo"
+                                                    onClick={() => showModal(question.heroId)}
+
+                                                >
+                                                    Infos
+                                                </button>
                                             </td>
                                                 
                                         </tr>
@@ -119,12 +196,82 @@ const QuizOver = React.forwardRef((props, ref) => {
                             )
     
     
+    const resultInModal = !loading ? (
+
+        <Fragment>
+        
+        <div className="modalHeader">
+                <h2>{characterInfos.data.results[0].name}</h2>                
+        </div>
+        <div className="modalBody">
+            <div className="comicImage">
+
+                <img 
+                    src={characterInfos.data.results[0].thumbnail.path+'.'+ characterInfos.data.results[0].thumbnail.extension} 
+                    alt={characterInfos.data.results[0].name} 
+                />
+
+                {characterInfos.attributionText}
+
+            </div>
+            <div className="comicDetails">
+
+                <h3>Description</h3>
+                {
+                    characterInfos.data.results[0].description ? 
+                <p>{characterInfos.data.results[0].description}</p>
+                : <p>Description indisponible...</p>
+                }
+                <h3>Plus d'infos</h3>
+
+                {
+                    characterInfos.data.results[0].urls && 
+                    characterInfos.data.results[0].urls.map((url, index) => {
+                    return <a 
+                        key={index}
+                        href={url.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {capitalizeFirestletter(url.type)}
+                    </a>
+                    })
+                }
+                
+
+            </div>
+        </div>
+        <div className="modalFooter" >
+            <button className="modalBtn" onClick={hideModal}>Fermer</button>
+        </div>
+        </Fragment>
+
+
+    )
+    :
+    (
+        <Fragment>
+        
+        <div className="modalHeader">
+                <h2>Réponse de Marvel...</h2>                
+        </div>
+        <div className="modalBody">
+            <Loader />
+        </div>
+        
+        </Fragment>
+
+    )
 
     return (
     <Fragment>
+
         {decision}
+
         <hr />
+
         <p>Les réponses aux questions posées: </p>
+
         <div className="answerContainer">
             <table className="answers">
                 <thead>
@@ -139,6 +286,13 @@ const QuizOver = React.forwardRef((props, ref) => {
                 </tbody>
             </table>
         </div>
+
+        <Modal showModal={openModal} hideModal={hideModal}>
+            
+            {resultInModal}
+
+        </Modal>
+
     </Fragment>
     )
 })
--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
  const config = {
-    apiKey: "AIzaSyAWWpNWbuisZvj6_Pzk6OAWA9fQQVyntrw",
-    authDomain: "marvel-quiz-7287f.firebaseapp.com",
-    databaseURL: "https://marvel-quiz-7287f.firebaseio.com",
-    projectId: "marvel-quiz-7287f",
-    storageBucket: "marvel-quiz-7287f.appspot.com",
-    messagingSenderId: "815857866303",
-    appId: "1:815857866303:web:696a765b0a6c0ac815ca8a"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
 class Firebase {
